ef527a50ff7cee256717135a28fc131d597dd603
greg <gregborrelly@gmail.com>
2017-05-16 11:13:19 -0400

    Setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name countDownApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the countDownApp
+ */
+angular.module('countDownApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
